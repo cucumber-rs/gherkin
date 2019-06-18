5c457331c0c0acb9e211eb406113f384389ee7cb
Brendan Molloy <brendan@bbqsrc.net>
2019-06-18 14:52:41 +0200

    Bump to v0.5.0

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,10 +1,10 @@
 #[macro_use]
-extern crate cucumber_rust;
+extern crate cucumber;
 use std::default::Default;
 
 pub struct MyWorld {}
 
-impl cucumber_rust::World for MyWorld {}
+impl cucumber::World for MyWorld {}
 impl Default for MyWorld {
     fn default() -> MyWorld {
         MyWorld {}
