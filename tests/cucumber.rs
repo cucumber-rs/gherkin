a57507858c1143083ffce6291afafbc1fc77e3c9
Brendan Molloy <brendan@bbqsrc.net>
2020-09-09 15:23:44 +0200

    Bump cucumber dep to 0.7

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,32 +1,38 @@
-#[macro_use]
 extern crate cucumber;
-use std::default::Default;
+
+use async_trait::async_trait;
+use std::convert::Infallible;
 
 pub struct MyWorld {}
 
-impl cucumber::World for MyWorld {}
-impl Default for MyWorld {
-    fn default() -> MyWorld {
-        MyWorld {}
+#[async_trait(?Send)]
+impl cucumber::World for MyWorld {
+    type Error = Infallible;
+
+    async fn new() -> Result<Self, Infallible> {
+        Ok(MyWorld {})
     }
 }
 
 mod t {
-    steps!(crate::MyWorld => {
-        given "I just started" |_world, _step| {
-            println!("HELO");
-        };
+    use cucumber::Steps;
 
-        when "Hello" |_world, _step| {
-
-        };
-    });
+    pub fn steps() -> Steps<crate::MyWorld> {
+        let mut builder: Steps<crate::MyWorld> = Steps::new();
+        builder
+    }
 }
 
-cucumber! {
-    features: "./tests/features",
-    world: crate::MyWorld,
-    steps: &[
-        t::steps
-    ]
+fn main() {
+    // Do any setup you need to do before running the Cucumber runner.
+    // e.g. setup_some_db_thing()?;
+
+    let runner = cucumber::Cucumber::<MyWorld>::new()
+        .features(&["./tests/features"])
+        .steps(t::steps());
+
+    // You may choose any executor you like (Tokio, async-std, etc)
+    // You may even have an async main, it doesn't matter. The point is that
+    // Cucumber is composable. :)
+    futures::executor::block_on(runner.run());
 }
